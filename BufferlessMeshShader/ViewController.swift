f94817d42e9e095fe87d9dce702593b01d6426ea
David Gavilan <david@endavid.com>
2024-01-23 17:50:09 +0000

    Initial Commit

---
+++ BufferlessMeshShader/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BufferlessMeshShader
+//
+//  Created by David Gavilan Ruiz on 23/01/2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
